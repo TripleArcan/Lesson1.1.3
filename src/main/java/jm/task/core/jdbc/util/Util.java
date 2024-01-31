11579f73eadac43c3189a37bf4e81543884a1b96
TripleArcan <nlogovchin@mail.ru>
2024-01-31 22:04:03 +0500

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -21,6 +21,7 @@ public class Util {
     public static void main(String[] args) {
 
     }
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -30,31 +31,34 @@ public class Util {
         }
         return connection;
     }
+
     public static SessionFactory getSessionFactory() {
         try {
-            Configuration configuration = new Configuration();
+            if (getSessionFactory == null) {
+                Configuration configuration = new Configuration();
 
-            Properties settings = new Properties();
-            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/lesson1.1.4");
-            settings.put(Environment.USER, "root");
-            settings.put(Environment.PASS, "Loga987321");
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/lesson1.1.4");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "Loga987321");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
-            settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.SHOW_SQL, "true");
 
-            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            settings.put(Environment.HBM2DDL_AUTO, "");
+                settings.put(Environment.HBM2DDL_AUTO, "");
 
-            configuration.setProperties(settings);
+                configuration.setProperties(settings);
 
-            configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(User.class);
 
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
 
-            getSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                getSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
